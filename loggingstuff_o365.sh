90b5bab1c9533cd58264c7348f9e7a6f0e1e226e
BoredHackerBlog <38662926+BoredHackerBlog@users.noreply.github.com>
2020-12-05 22:25:05 -0500

    Create loggingstuff_o365.sh

---
+++ loggingstuff_o365.sh
@@ -0,0 +1,6 @@
+wget "https://github.com/counteractive/o365beat/releases/download/v1.5.1/o365beat-1.5.1-linux-x86_64.tar.gz"
+tar xvfs o365beat-1.5.1-linux-x86_64.tar.gz
+cd o365beat-7.5.1-linux-x86_64
+echo "Replace o365beat.yml with your file"
+echo "Run ./o365beat test config -e to test the config"
+echo "Run ./o365beat" to start running
